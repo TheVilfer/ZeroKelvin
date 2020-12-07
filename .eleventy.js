6e0b2b00b12a31c6050ef9d2e5edd36937b1ee72
vilfer <sergey.polin@urfu.me>
2020-12-08 01:02:12 +0500

    add 11ty config

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = (config) => {
+    config.addPassthroughCopy('src/favicon.ico');
+    config.addPassthroughCopy('src/fonts');
+    config.addPassthroughCopy('src/images');
+    config.addPassthroughCopy('src/scripts');
+    config.addPassthroughCopy('src/styles');
+    return {
+        dir: {
+            input: 'src',
+            output: 'dist',
+            includes: 'includes',
+            layouts: 'layouts'
+        },
+        dataTemplateEngine: 'njk',
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        passthroughFileCopy: true,
+        templateFormats: [
+            'md', 'njk'
+        ],
+    };
+  };

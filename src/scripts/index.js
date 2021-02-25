91f0e9b97fe854a076df11ab5bcd4eb6a9d2b6dc
vilfer <sergey.polin@urfu.me>
2021-02-25 19:42:11 +0500

    Разработка. Исправление ошибок

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,29 @@
-var mySwiper = new Swiper('.swiper-container', {
+
+var mySwiper = new Swiper('.swiper-banner', {
     speed: 400,
-    spaceBetween: 100
+    spaceBetween: 100,
+    loop: true,
+    keyboard: {
+      enabled: true,
+    },
 });
 
-var nav = document.querySelector('.nav');
+var collectionsSwiper = new Swiper('.swiper-collections', {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop:true,
+  keyboard: {
+    enabled: true,
+  },
+});
+
+var catalogSwiper = new Swiper('.swiper-catalog', {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop:true,
+  keyboard: {
+    enabled: true,
+  },
+});

efe787d87407c85133b85e4210e8602bcdfc00e2
vilfer <sergey.polin@urfu.me>
2021-03-11 19:47:49 +0500

    Разработка. Добавляет favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,36 @@
 module.exports = (config) => {
-    //config.addPassthroughCopy('src/favicon.ico');
+    config.addPassthroughCopy('src/favicon.ico');
+    config.addPassthroughCopy('src/android-chrome-192x192.png');
+    config.addPassthroughCopy('src/android-chrome-512x512.png');
+    config.addPassthroughCopy('src/apple-touch-icon-57x57-precomposed.png');
+    config.addPassthroughCopy('src/apple-touch-icon-57x57.png');
+    config.addPassthroughCopy('src/apple-touch-icon-60x60-precomposed.png');
+    config.addPassthroughCopy('src/apple-touch-icon-60x60.png');
+    config.addPassthroughCopy('src/apple-touch-icon-72x72-precomposed.png');
+    config.addPassthroughCopy('src/apple-touch-icon-72x72.png');
+    config.addPassthroughCopy('src/apple-touch-icon-76x76-precomposed.png');
+    config.addPassthroughCopy('src/apple-touch-icon-76x76.png');
+    config.addPassthroughCopy('src/apple-touch-icon-114x114-precomposed.png');
+    config.addPassthroughCopy('src/apple-touch-icon-114x114.png');
+    config.addPassthroughCopy('src/apple-touch-icon-120x120-precomposed.png');
+    config.addPassthroughCopy('src/apple-touch-icon-120x120.png');
+    config.addPassthroughCopy('src/apple-touch-icon-144x144-precomposed.png');
+    config.addPassthroughCopy('src/apple-touch-icon-144x144.png');
+    config.addPassthroughCopy('src/apple-touch-icon-152x152-precomposed.png');
+    config.addPassthroughCopy('src/apple-touch-icon-152x152.png');
+    config.addPassthroughCopy('src/apple-touch-icon-180x180-precomposed.png');
+    config.addPassthroughCopy('src/apple-touch-icon-180x180.png');
+    config.addPassthroughCopy('src/apple-touch-icon-precomposed.png');
+    config.addPassthroughCopy('src/apple-touch-icon.png');
+    config.addPassthroughCopy('src/browserconfig.xml');
+    config.addPassthroughCopy('src/favicon-16x16.png');
+    config.addPassthroughCopy('src/favicon-32x32.png');
+    config.addPassthroughCopy('src/mstile-150x150.png');
+    config.addPassthroughCopy('src/safari-pinned-tab.svg');
+    config.addPassthroughCopy('src/site.webmanifest');
+
+
+
     config.addPassthroughCopy('src/fonts');
     config.addPassthroughCopy('src/images');
     config.addPassthroughCopy('src/scripts');
--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -30,21 +30,20 @@ var catalogSwiper = new Swiper('.swiper-catalog', {
 
 //cart
 document.addEventListener('DOMContentLoaded', () =>{
+  localStorage.setItem('storage', JSON.stringify({}));
   const productBtn = document.querySelectorAll('.item__cart__test');
   productBtn.forEach(el => {
     el.addEventListener('click', (e) =>{
       let self = e.currentTarget;
       let parent = self.closest('.catalog__item');
       let id = parent.dataset.id;
-      addStorage(id);
+      addCart(id);
     }); 
   });
 });
 
-const addStorage = (id) =>{
-  if (localStorage.getItem(id) != null){
-    localStorage.setItem(id, parseInt(localStorage.getItem(id))+1)
-  } else{
-    localStorage.setItem(id, '1')
+const addCart = (id) =>{
+  localStorage.setItem('storage', JSON.stringify({id:"1"}))
+  let str = JSON.parse(localStorage.storage);
+  console.log(str);
   }
-}; 

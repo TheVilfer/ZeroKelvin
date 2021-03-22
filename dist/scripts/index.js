7215a5bbd1cd4ea7d72d243cae0a9461128bbdc2
vilfer <sergey.polin@urfu.me>
2021-03-22 23:07:05 +0500

    netlify test

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -82,7 +82,8 @@ const getAmoutPrice = (cart) => {
   total = 0
   cart.forEach(el => {
     let item = sendRequestCart(el[0]);
-    total += item[0].price
+    console.log(item)
+    // total += item[0].price
   });
   console.log(total)
   // return total;

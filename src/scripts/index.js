f2f0d0df966df05b95ee4c251cb1d6bafd2840c5
vilfer <sergey.polin@urfu.me>
2021-03-23 23:32:32 +0500

    netlify test

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -35,7 +35,10 @@ const catalogSwiper = new Swiper('.swiper-popular', {
 document.addEventListener('DOMContentLoaded', () => {
   if (localStorage.getItem('cart') == null) localStorage.setItem('cart', JSON.stringify({}));
   const productBtn = document.querySelectorAll('.popular-item__cart');
-  if (window.location.pathname == "/cart/") updateCart();
+  if (window.location.pathname == "/cart/") {
+    updateCart();
+
+  };
   productBtn.forEach(el => {
     el.addEventListener('click', (e) => {
       let self = e.currentTarget;
@@ -84,10 +87,19 @@ const getAmoutPrice = async (cart) => {
   total = 0
   for (const el of cart) {
     let item = await sendRequestCart(el[0]);
+    // RenderCart(item[0], el[1]);
     total += item[0].price * el[1];
   }
   return total;
 };
-const RenderCart = (item) => {
-
+const RenderCart = (item, count) => {
+  let cart = document.querySelector('.cart');
+  const el = `<li class="cart-element" data-id="${item._id}">
+            <span class="cart-element__count">${count}x</span>
+            <img class="cart-element__image" src="/images/products/${item._id}.jpg" alt="">
+            <span class="cart-element__name">${item.name}</span>
+            <span class="cart-element__price">${item.price}</span>
+            <button class="cart-element__delete">Удалить</button>
+        </li>`;
+  cart.insertAdjacentHTML('afterbegin', el);
 };

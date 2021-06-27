239a6a700b8c2cf265930a816dc0d0579f224701
vilfer <sergey.polin@urfu.me>
2021-06-27 21:25:23 +0500

    Разработка. Исправление gitignore

--- dist/scripts/dist/index.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var mySwiper = new Swiper('.swiper-container', {
-  speed: 400,
-  spaceBetween: 100
-});
-var nav = document.querySelector('.nav');
--- dist/scripts/dist/index.prod.js
+++
@@ -1,1 +0,0 @@
-"use strict";var mySwiper=new Swiper(".swiper-container",{speed:400,spaceBetween:100}),nav=document.querySelector(".nav");

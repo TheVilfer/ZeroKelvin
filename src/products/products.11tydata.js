0c20d63b1f4e5bc0c0b280b6773421bd0cf30cf4
vilfer <sergey.polin@urfu.me>
2021-08-05 18:07:52 +0500

    Фикс. Добавляет минорное исправление

--- src/products/products.11tydata.js
+++ src/products/products.11tydata.js
@@ -4,15 +4,16 @@ const path = require("path");
 const getImages = (url, image) => {
   let dirFiles = [];
   let artworks = "";
-  if (image != undefined) artworks = path.basename(image);
-  let tt = path.resolve(path.dirname(url), "images/");
-  fs.readdirSync(tt).forEach((file) => {
-    if (file == ".DS_Store") return;
-    if (file != artworks) {
-      dirFiles.push(path.join("images/", file));
-      console.log(path.join("images/", file));
-    }
-  });
+  if (image != undefined) {
+    artworks = path.basename(image);
+    let tt = path.resolve(__dirname, path.dirname(image));
+    fs.readdirSync(tt).forEach((file) => {
+      if (file == ".DS_Store") return;
+      if (file != artworks) {
+        dirFiles.push(path.join("images/", file));
+      }
+    });
+  }
   return dirFiles;
 };
 

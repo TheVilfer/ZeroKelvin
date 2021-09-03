c7d3ae52cf014385decf857c68e38f86c1d67bdb
vilfer <sergey.polin@urfu.me>
2021-09-03 22:30:29 +0500

    Разработка. Фиксит баг

--- src/functions/test.js
+++ src/functions/test.js
@@ -16,7 +16,19 @@ const Amo = require("../scripts/modules/AmoLibrary.js");
 const DbCore = require("../scripts/modules/dbCore.js");
 module.exports.handler = async (event, context) => {
   await Amo.Init("tokens", "60c0e125e35a6baee25a652e");
-  const resp = await Amo.Get("/api/v4/leads/12280437?with=catalog_elements");
+  const resp = await Amo.Patch(`/api/v4/catalogs/${8693}/elements/${1175381}`, {
+    name: `Счет к заказу #${1175381}`,
+    custom_fields_values: [
+      {
+        field_id: 982493,
+        values: [
+          {
+            value: "Оплачен",
+          },
+        ],
+      },
+    ],
+  });
   return {
     statusCode: 200,
     body: JSON.stringify(resp),

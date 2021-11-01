978398c3c462578d6f35f09cbd8c90db9add290d
vilfer <sergey.polin@urfu.me>
2021-11-01 22:52:24 +0500

    Quick fix

--- src/_data/banners.js
+++ src/_data/banners.js
@@ -1,10 +1,11 @@
 module.exports = function () {
   let data = require("../subdata/banners.json");
   for (let index = 0; index < data.banners.length; index++) {
-    data.banners[index].img = data.banners[index].img.replace("src", "");
+    data.banners[index].img = data.banners[index].img.replace("src/", "");
     data.banners[index].img_compressed = data.banners[
       index
-    ].img_compressed.replace("src", "");
+    ].img_compressed.replace("src/", "");
   }
+
   return data.banners;
 };

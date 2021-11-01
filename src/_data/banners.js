4fdabbc1903ddcdd21ec87c5dfc3d18857781a3b
vilfer <sergey.polin@urfu.me>
2021-11-01 22:42:18 +0500

    Разработка. Изменяет ссылку

--- src/_data/banners.js
+++ src/_data/banners.js
@@ -1,4 +1,10 @@
 module.exports = function () {
   let data = require("../subdata/banners.json");
+  for (let index = 0; index < data.banners.length; index++) {
+    data.banners[index].img = data.banners[index].img.replace("src", "");
+    data.banners[index].img_compressed = data.banners[
+      index
+    ].img_compressed.replace("src", "");
+  }
   return data.banners;
 };
